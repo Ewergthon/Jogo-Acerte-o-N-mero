d95bf023ff494366d5dda3eafae3d9dbdeb3ebad
Ewergthon Renan <121515229+Ewergthon@users.noreply.github.com>
2025-02-10 13:45:51 -0300

    Criação de verificação de chute e troca de texto.

--- app.js
+++ app.js
@@ -10,7 +10,17 @@ exibirTextoNaTela('p','Escolha um número entre 1 e 100')
 
 function verificarChute(){
     let tentativa = document.querySelector('input').value;
-    console.log(tentativa == numeroSecreto);
+    
+    if (tentativa == numeroSecreto){
+        exibirTextoNaTela('h1', 'Acertou!! Parabéns');
+        exibirTextoNaTela('p', 'Você descobriu o número secreto.');
+    } else {
+        if (tentativa > numeroSecreto){
+            exibirTextoNaTela('p', 'Número secreto é menor que o chute.');
+        } else {
+            exibirTextoNaTela('p', 'O número secreto é maior que o chute.')
+        }
+    }
 }
 
 function gerarNumeroAleaorio(){
